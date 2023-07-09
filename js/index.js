f888cf5823706d00dbaf7bca78bcffe2c7459aa6
pstnv <pstnv@mail.ru>
2023-07-09 15:37:45 +0500

    add form and section message

--- js/index.js
+++ js/index.js
@@ -17,3 +17,52 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+
+const messageForm = document.querySelector('form[name="leave_message"]');
+const messageSection = document.querySelector('#messages');
+const messageList = messageSection.querySelector('ul');
+
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const userName = form.usersName.value;
+    const useEmail = form.usersEmail.value;
+    const userMessage = form.usersMessage.value;
+
+    // console.log(userName);
+    // console.log(useEmail);
+    // console.log(userMessage);
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `
+        <a href="mailto:${useEmail}" target="_blank">${userName}</a>
+        wrote: <span> ${userMessage}</span>
+    `;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    form.reset();
+});
+
+messageList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const button = e.target;
+        const li = button.parentNode;
+        const action = button.textContent;
+
+        const nameActions = {
+            remove: () => {
+                li.remove();
+            }
+        }
+        nameActions[action]();
+    }
+});
