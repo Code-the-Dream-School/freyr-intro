46e652a45c056979e4fb50d97991c6db1142ae10
roman712 <116198267+roman712@users.noreply.github.com>
2023-08-02 17:01:45 -0500

    lesson-4-2

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var today = new Date();
+var thisYear = today.getFullYear();
+
+var footer = document.querySelector("footer");
+
+var copyright = document.createElement("P");
+
+copyright.innerHTML = "Roman" + thisYear;
+
+document.body.appendChild(copyright);
+
+var skills = ["JavaScript", "Html", "Css", "Git"];
+
+var skillsSection = document.getElementById("skills");
+
+//skills section to find the <ul> element
+var skillsList = skillsSection.querySelector("ul");
+
+//loop to iterate over the skills array
+for (var i = 0; i < skills.length; i++) {
+  //creating a new list item li element
+  var skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
