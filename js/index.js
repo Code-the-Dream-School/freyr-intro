2e7c73a6505303eba85392ede1356de71215aa8a
NatalyBMota <interactivedesign@yahoo.com>
2023-08-02 22:32:36 -0500

    Added functionality to ensure that the save and edit buttons work repeatedly. In order for that to work, I had to stop removing the save button and the edit button, then recreating it; and start hiding and unhiding the save and the remove buttons instead.

--- js/index.js
+++ js/index.js
@@ -142,6 +142,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         let editButton = createButtonAndAppendIt('edit');
+        let saveButton = createButton('save');
         let removeButton = createButtonAndAppendIt('remove');
         /*
             The above code is the same as the following code, in a refactored form:
@@ -161,13 +162,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
         messageList.appendChild(newMessage);
 
+        /*
+        newMessage.addEventListener('click', (e) => {
+            if (e.target.tagName === 'BUTTON') {
+                const button = e.target;
+                let entry = button.parentNode;
+                // The variable entry is a list item (li), which is the element that is 
+                //the parent of the button.
+                //const ul = entry.parentNode;
+                // It's parent is the unordered list (ul), which is stored in the 
+                //variable messageList.
+
+                if (button.textContent === 'remove') {
+                    entry.remove();
+                    let numOfListItems = messageList.childElementCount;
+                    //console.log(numOfListItems);
+                    if (numOfListItems <= 1) {
+                        messageSection.style.display = 'none';
+                    } 
+                } else if (button.textContent === 'edit') {
+                    //let saveButton = createButton('save');
+
+                    let messageContainerSpan = newMessage.getElementsByTagName('span')[0];
+                    let messageText = messageContainerSpan.textContent;
+                    messageContainerSpan.textContent = '';
+
+                    let editInputField = document.createElement('input');
+                    editInputField.type = 'text';
+                    editInputField.id = 'editInputField';
+                    editInputField.value = messageText;
+
+                    newMessage.insertBefore(editInputField, editButton);
+                    //newMessage.insertBefore(saveButton, removeButton);
+                    //editButton.remove();
+                } else if (button.textContent === 'save') { 
+                    
+                }
+            }
+        });
+        */
+        
         removeButton.addEventListener('click', () => {
             let entry = removeButton.parentNode;
-            /* 
-                The variable entry is a list item (li), which is the element that is the parent of the editButton. 
-                It's parent is the unordered list (ul), which is stored in the variable messageList.
-                The ul's parent is the section element with the id of messages.
-            */
+            
+                //The variable entry is a list item (li), which is the element that is 
+                //the parent of the editButton. 
+                //It's parent is the unordered list (ul), which is stored in the 
+                //variable messageList.
+                //The ul's parent is the section element with the id of messages.
+            
             let numOfListItems = messageList.childElementCount;
             console.log(numOfListItems);
             entry.remove();
@@ -176,9 +219,8 @@ document.addEventListener('DOMContentLoaded', () => {
             } 
         });
 
-        let saveButton = createButton('save');
-
         editButton.addEventListener('click', () => {
+            saveButton.style.display = '';
             let messageContainerSpan = newMessage.getElementsByTagName('span')[0];
             let messageText = messageContainerSpan.textContent;
             messageContainerSpan.textContent = '';
@@ -191,11 +233,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
             
             newMessage.insertBefore(saveButton, removeButton);
-            editButton.remove();
+            editButton.style.display = 'none'; 
+            //editButton.remove();
 
         });
-
+        
         saveButton.addEventListener('click', () => {
+            editButton.style.display = '';
             let messageContainerSpan = newMessage.getElementsByTagName('span')[0];
             let editInputField = document.getElementById('editInputField');
             let messageText = editInputField.value;
@@ -204,9 +248,10 @@ document.addEventListener('DOMContentLoaded', () => {
             
             editInputField.remove();
 
-            let editButton = createButton('edit');
+            //let editButton = createButton('edit');
             newMessage.insertBefore(editButton, removeButton);
-            saveButton.remove();
+            saveButton.style.display = 'none';
+            //saveButton.remove();
         });
         messageForm.reset();
     });
