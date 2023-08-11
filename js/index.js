6ea7e31215a68355494de91b496ab8d88ff37c70
annaUniversal <anna.universal@yahoo.com>
2023-08-10 20:52:25 -0400

    added form and remove button

--- js/index.js
+++ js/index.js
@@ -3,12 +3,54 @@ var skills = ["JavaScript", "HTML", "CSS", "DOM"]; // technical skills
 const skillsSection = document.getElementById('skills'); // selecting the section with id skills
 const skillsList = skillsSection.querySelector('ul');
 
-// creating an array of skils
+// Creating an array of skils
 for (let i = 0; i < skills.length; i++){
     let skill = document.createElement('li');
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 };
+
+
+
+
+// Email form handeling
+const messageForm = document.getElementsByName('leaveMessege')[0];
+messageForm.addEventListener('submit', (event) =>{
+    event.preventDefault(); 
+   // grabing data from the form
+    const usersName = event.target.userName.value;
+    const usersEmail = event.target.userEmail.value;
+    const userMessage = event.target.userMessage.value;
+
+    console.log(usersName, usersEmail, userMessage);
+
+    // Selection for the Messages List
+    const messageSection = document.getElementById('message');
+    //const messageList = document.getElementById('messageList');
+    const messageList = messageSection.getElementsByTagName(ul);
+    //const newMessage = document.createElement('li');
+    const newMessage = document.createElement('li');
+    
+    // Output on the page the user's message
+    newMessage.innerHTML =`<a href="mailto:${usersEmail}">${usersName}: </a> wrote:  <span>${userMessage}</span> `
+    
+    // Remove button creation and handling
+    const removeButton = document.createElement('button')
+    removeButton.innerHTML = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', (event) => {
+        //messageList.removeChild(newMessage);
+        const entry = removeButton.parentNode;
+        entry.remove(); 
+     });
+
+    newMessage.appendChild(removeButton);
+    messageList.append(newMessage);
+
+    event.target.reset();
+});
+
 
 
 // Adding copyright in the footer
@@ -20,4 +62,3 @@ const footerContent = document.createElement('p');
 const textC = `<small>&#169; Anna Gerhardt ${thisYear}</small>`;
 footerContent.innerHTML = textC;
 containerF.appendChild(footerContent);
-
