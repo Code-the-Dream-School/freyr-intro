adcad1b4f5b65d78cc4972980284512eda8a6724
Aysa <aysabar92@gmail.com>
2023-08-12 22:55:47 -0400

    JS file updated

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  const today = new Date();
  const thisYear = today.getFullYear();
 
- const footer = document.querySelector('#footer');
+ const footer = document.querySelector('footer');
  
  const copyright = document.createElement('p');
  
@@ -26,11 +26,58 @@
  ];
 
  const skillsSection = document.getElementById('skills');
+ 
  const skillsList = skillsSection.querySelector('ul');
 
-for (let i = o; i < skills.length; i ++) {
+for (let i = 0; i < skills.length; i ++) {
     const skill = document.createElement('li');
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 
 };
+
+
+
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const usersName = e.target.usersName.value;
+    console.log(usersName);
+    const usersEmail = e.target.usersEmail.value;
+    console.log(usersEmail);
+    const usersMessage= e.target.usersMessage.value;
+    console.log(usersMessage);
+
+    const messageSection = document.getElementById('messages');
+    let messageList = messageSection.querySelector('ul');
+    let newMessage = document.createElement('li');
+    newMessage.innerHTML =  `<a href=mailto: ${usersEmail}> ${usersName}</a>
+                         <span >${usersMessage}</span>`
+
+    messageList.appendChild(newMessage);
+                         
+    let removeButton = document.createElement('button');
+    removeButton.innerText = "Remove";
+    removeButton.type = 'button';
+
+    newMessage.appendChild(removeButton);
+    removeButton.addEventListener('click', (e) => {
+    
+        let entry = removeButton.parentNode;
+        entry.remove();
+
+        if (messageList.value) {
+            messageSection.style.display = 'Messages';
+         } else {
+            messageSection.style.display = 'none';
+         }
+        
+    });
+
+   
+   
+
+    messageForm.reset();
+});
+
