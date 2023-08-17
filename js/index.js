93141e146565e960425fd423cd146f979ae594b8
canilo <carlos.will51@gmail.com>
2023-08-17 13:40:36 -0700

    Updating Animation JS Text
    Text Animation

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const SkillsSection = document.getElementById('skills')
 const SkillsList = SkillsSection.querySelector('ul')
 const messageSection = document.getElementById('messages')
 const messageList =  messageSection.querySelector('ul')
-
+const Button = document.getElementById('Circle')
 
 //RENDER FUNCTIONS
 function renderCopyRight(){
@@ -27,6 +27,34 @@ function renderSkills(){
       SkillsList.appendChild(skill)
    }
 }
+function AboutMeAnimation() {
+   const aboutMe = document.getElementsByName('Animation1')[0];
+   const text = aboutMe.innerText;
+   console.log(text);
+   aboutMe.innerText = "";
+
+   function animateText(index) {
+      if (index < text.length) {
+         if (text[index] === ' ') {
+            setTimeout(function() {
+               aboutMe.innerText += ' ';
+               console.log("it found the space: ", aboutMe.innerText)
+               animateText(index + 1);
+            }, 700);
+         } else {
+            aboutMe.innerText += text[index];
+            setTimeout(function() {
+               animateText(index + 1);
+            }, 100); 
+         }
+      }
+   }
+
+   animateText(0);
+}
+
+// Call the animation function
+
 
 
 //EVENT HANDLERS
@@ -41,6 +69,7 @@ function submit(event){
 
    newMessage.setAttribute("id","Inbox")
    newMessage.innerHTML = `<a> ${Name} ${Email} ${usersMessage}<a/a> <span></span>`
+   
    messageList.appendChild(newMessage)
    //clear all inputs
    event.target.reset()
@@ -49,11 +78,15 @@ function submit(event){
 function entry(event){
    removebutton.remove()
 }
+function Click(event){
+   console.log("This is the event: ",event)
+}
 
 
 //LOADINGPAGE
 renderCopyRight()
 renderSkills()
+AboutMeAnimation()
 messageForm.addEventListener("submit",submit)
 
 
