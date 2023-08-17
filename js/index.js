f72cd36096eac20afd96849cd9da59dd8da94496
Chrisopher <studio1290@Chrisophers-MacBook-Pro.local>
2023-08-17 01:26:09 -0500

    I hope everything is okay

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+
+
+messageForm.addEventListener('submit', function(event){
+	event.preventDefault();
+	const formData = new FormData(event.target); //retrive input data from the html file
+
+	const usersName = formData.get('usersName');
+	const usersEmail = formData.get('usersEmail');
+	const usersMessage = formData.get('usersMessage');
+
+	console.log('Name', usersName);
+	console.log('Email', usersEmail);
+	console.log('Message', usersMessage);
+	
+	const messageSection = document.getElementById('messages');
+	const messageList = messageSection.querySelector('ul');
+	
+	const newMessage = document.createElement('li');
+	newMessage.innerHTML =`
+		
+		<a href='mailto:${usersEmail}'>${usersName}</a>
+		<span>${usersMessage}</span>
+	`;
+	const removeButton = document.createElement('button');
+	removeButton.innerText = 'Remove';
+	removeButton.type = 'button';
+	removeButton.addEventListener('click', function(){
+		const entry = removeButton.parentNode;
+		entry.remove();
+	});
+	
+	newMessage.appendChild(removeButton);
+	messageList.appendChild(newMessage);
+
+	messageForm.reset();
+
+});
+
+
+
 const skills = ["Ear-training","Practical Technique","Aural-training","Music Analysis", "Historical Studies in Classical Music"];
 
 const skillsSlection = document.getElementById("skills");
@@ -5,10 +47,10 @@ const skillsSlection = document.getElementById("skills");
 const skillsList = skillsSlection.querySelector("ul");
 
 for (let i = 0; i < skills.length; i++){
-	console.log(skills[i]);
-	const skill = document.createElement("li");
-	skill.textContent = skills[i];
-	skillsList.appendChild(skill);
+	console.log(skills[i]); //print out each skill from the skills list
+	const skill = document.createElement("li"); // create an list of element named skill
+	skill.textContent = skills[i]; // append the name of each skill to the each skill element
+	skillsList.appendChild(skill); // append each skill element (with the name of each skill) to the list 'skillsList'
 }
 
 const today = new Date();
