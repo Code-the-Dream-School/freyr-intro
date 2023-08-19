c252bafb45a1c703b81bb974d5cbfb7100da3eb9
OleksandraNy <sandra.nyk@gmail.com>
2023-08-19 10:32:23 -0700

    changes to html and js files for lesson 4.4

--- js/index.js
+++ js/index.js
@@ -18,3 +18,30 @@ for (i = 0; i < skills.length; i++) {
 skill.innerText=skills[i];
 skillsList.appendChild(skill);
 }
+
+const messageForm=document.getElementById("leave_message");
+messageForm.addEventListener('submit',  (event)=> {
+event.preventDefault();
+const nameValue=event.target.name.value;
+const emailValue=event.target.email.value;
+const messageValue=event.target.message.value;
+console.log(nameValue);
+console.log(emailValue);
+console.log(messageValue);
+const messageSection=document.getElementById("messages");
+const messageList= messageSection.querySelector('ul');
+const newMessage=document.createElement('li');
+newMessage.innerHTML=`<a href="mailto:${emailValue}">${nameValue}</a> <span> ${messageValue} </span>`;
+console.log(newMessage)
+messageForm.reset()
+const removeButton=document.createElement('button');
+removeButton.innerText='remove';
+console.log(removeButton);
+removeButton.type="button";
+removeButton.addEventListener('click',  (remove)=>{
+    const entry=removeButton.parentNode;
+    entry.remove();
+})
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
+})
