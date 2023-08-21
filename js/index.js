44775e615d075f73e09824d8880909c788787cf0
StephAlvarado <Stephya16@gmail.com>
2023-08-21 17:07:55 -0600

    added various elements to index.js and index.html

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
-let today = new Date()
-let thisYear = today.getFullYear()
+const today = new Date();
+let thisYear = document.getFullYear();
+let myName = 'Stephanie Alvarado';
+const footer = document.querySelector('#footer');
+ 
+const copyright = document.createElement('p');
+
+copyright.innerHTML = 'myName.thisYear';
+footer.appendChild(copyright);
+
+
+let skills = [Microsoft, Javascript, html, CSS, Lightroom];
+const skillsSection = document.getElementById('skills');
+const skillsList = document.querySelector(skillsSection);
+
+for (let i = 0; i < skills.length; i++) {
+   let skill = document.createElement('li');
+    text += skills[i] + "<li>";
+    skillsList.appendChild(skill);
+  }
+
+
+
+
+
+
+
