085c9d589a38b75317670e210c895644096fa06e
Cristian Moran <cmoran1654@gmail.com>
2023-08-26 12:42:58 -0500

    commit message

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const copyright = document.createElement("p");
 
 //copyright logo
 const logoCopyright = document.createElement("p");
-logoCopyright.innerHTML = "&#9881";
+logoCopyright.innerHTML = "&#128526";
 logoCopyright.style.display = "inline";
 footer.appendChild(logoCopyright);
 
@@ -22,16 +22,16 @@ headings.forEach(function (heading) {
 //this is a variable refering to the buttonContainer
 var container = document.querySelector("nav button");
 
-container.addEventListener("click", function (event) {
-  var clicked = event.target.id;
-  if (clicked == "button" || "About") {
-    alert("Welcome to my page!");
-  } else if (clicked == "button" || "Experience") {
-    alert("None lol");
-  } else if (clicked == "button" || "button") {
-    alert("Follow for follow?");
-  }
-});
+// container.addEventListener("click", function (event) {
+//   var clicked = event.target.id;
+//   if (clicked == "button" || "About") {
+//     alert("Welcome to my page!");
+//   } else if (clicked == "button" || "Experience") {
+//     alert("None lol");
+//   } else if (clicked == "button" || "button") {
+//     alert("Follow for follow?");
+//   }
+// });
 
 //skills section
 const skills = [
