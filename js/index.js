9131be332854b3eea3227c4e06ed890abe8b16ad
Nataly B Mota <interactivedesign@yahoo.com>
2023-09-04 21:38:07 -0500

    Added the strong tag containing the link with the user name, the word 'wrote' followed by a colen, and the message that the user typed into the form, to its own div, by wrapping the HTML content of the newMessage variable, with a div tag that has the ID attribute of startingFlexItems. Also added a space after the anchor tag that contains a link to the user's email, and that displays the name of the user, using a special technique; because it was refusing to display normally otherwise. Did so by using the &nbsp character entity to make it appear where it should.

--- js/index.js
+++ js/index.js
@@ -199,9 +199,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let messageList = messageSection.querySelector("ul");
     let newMessage = document.createElement("li");
-    //newMessage.innerHTML = `<strong><a href='mailto:${email}'>${name}</a> wrote:</strong> <span>${message}</span>`;
-    newMessage.innerHTML = `<strong><a href='mailto:${email}'>${name}</a> wrote:</strong>&nbsp;`;
-    newMessage.innerHTML += `${createSpanWithMessage(message)}`;
+    //newMessage.innerHTML = `<strong><a href='mailto:${email}'>${name}</a>&nbsp; wrote:</strong> <span>${message}</span>`;
+    newMessage.innerHTML = `<div id="startingFlexItems"><strong><a href='mailto:${email}'>${name}</a> &nbsp;`;
+    newMessage.innerHTML += `wrote:</strong>&nbsp; ${createSpanWithMessage(message)}</div>`;
 
     function createButton(buttonText) {
       let typeOfButton = document.createElement("button");
