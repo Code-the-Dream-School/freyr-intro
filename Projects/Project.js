ed94e0bcb1e0f6b56f1c07c8eb00ccfaa882357b
canilo <carlos.will51@gmail.com>
2023-09-18 15:59:39 -0700

    6.2
    Finish 6.2

--- Projects/Project.js
+++ Projects/Project.js
@@ -10,47 +10,40 @@ console.log("this is,",Programheader)
 const Games = document.getElementById("GamesButton")
 console.log("This is", Games)
 const RobloxTab = document.getElementById("RobloxTab")
-console.log("This is", RobloxTab)
-var githubRequest = new XMLHttpRequest();
-githubRequest.open("GET", "https://api.github.com/users/canilo1/repos");
-console.log(githubRequest)
-var repositories; // Declare the repositories variable in a higher scope
+console.log("This is", RobloxTab) 
 
-var githubRequest = new XMLHttpRequest();
-githubRequest.open("GET", "https://api.github.com/users/canilo1/repos");
-
-// Set up an event listener for the 'load' event
-githubRequest.addEventListener('load', function (event) {
-  // Check if the request was successful (status code 200)
-  if (githubRequest.status === 200) {
-    // Parse the response and store it in the 'repositories' variable
-    repositories = JSON.parse(this.responseText);
-    
-    // Log the value of 'repositories' in the console
-    console.log("repo:", repositories);
-
-    // Now you can access the 'repositories' variable outside this function
- 
-    var projectSection = document.getElementById("GithubProjects");
-    console.log("This is the project section", projectSection)
-    const projectList = projectSection.querySelector("ul");
-    
-    console.log("This is the project: ", projectList)
-    for (let i = 0; i < repositories.length; i++) {
-      console.log("This is i", i, "This is the current repository", repositories[i], "Finally, this is the repositories length", repositories.length);
-      console.log(repositories[i].name)
-      var project = document.createElement("li")
-        project.innerText = repositories[i].name
-        projectList.appendChild(project)
+var GitHubPromise = fetch("https://api.github.com/users/canilo1/repos");
+GitHubPromise
+  .then(function(Response){
+    var GithubStatus = Response.status
+    if(GithubStatus == 200){
+      console.log("This is the status", Response.status)
+      return Response.json();
+    }else{
+      console.error('Error: Request failed with status', GithubStatus);
     }
-   
-  } else {
-    // Handle any errors or non-200 status codes here
-    console.error('Error: Request failed with status', githubRequest.status);
-  }
-});
+  })
 
-githubRequest.send();
+  .then(function(RepositoryData){
+    console.log("This is the repo data",RepositoryData)
+    console.log("Repos.status",RepositoryData.status)
+    // return RepositoryData
+          // Log the value of 'repositories' in the console
+          // Now you can access the 'repositories' variable outside this function
+          var projectSection = document.getElementById("GithubProjects");
+          console.log("This is the project section", projectSection)
+          const projectList = projectSection.querySelector("ul");
+          console.log("This is the project: ", projectList)
+          for (let i = 0; i < RepositoryData.length; i++) {
+            console.log("This is i", i, "This is the current repository", RepositoryData[i], "Finally, this is the repositories length", RepositoryData.length);
+            console.log(RepositoryData[i].name)
+            var project = document.createElement("li")
+              project.innerText = RepositoryData[i].name
+              projectList.appendChild(project)
+          }
+         
+        });
+// console.log(githubRequest)
 
 function ButtonToggleVisibility() {
     // Select all elements with the class "Hidden"
@@ -69,58 +62,52 @@ function ToggleRoblox(){
     console.log("Clicked it!")
     const ROBLOX = document.getElementById("ROBLOX")
     console.log("This is roblox", ROBLOX)
-    ROBLOX.style.opacity = 1 
+    ROBLOX.classList.toggle("Hidden")
     console.log("Roblox's opacity is: ",  ROBLOX.style.opacity)
     const Website = document.getElementById('WebsiteInterface')
-    Website.style.opacity = 0
+Website.classList.add("Hidden")
     const Programs = document.getElementById("ProgramsInterface")
-    Programs.style.opacity = 0
+    Programs.classList.add("Hidden")
 }
 function ToggleWebsite() {
     console.log("Clicked Website!");
     // Hide other sections
     const ROBLOX = document.getElementById("ROBLOX");
-    ROBLOX.style.opacity = 0
+    ROBLOX.classList.add("Hidden")
     const Programs = document.getElementById("ProgramsInterface");
-    Programs.style.opacity = 0
+    Programs.classList.add("Hidden")
     // Show the Website section
     const Website = document.getElementById('WebsiteInterface');
-    Website.style.opacity = 1
+    Website.classList.toggle("Hidden")
 }
 
 function ToggleProject(){
     console.log("they clicked2")
     const ROBLOX = document.getElementById("ROBLOX")
-    ROBLOX.style.opacity = 0
+    ROBLOX.classList.add("Hidden")
     const Website = document.getElementById('WebsiteInterface')
-    Website.style.opacity = 0
+  Website.classList.add("Hidden")
     const Programs = document.getElementById("ProgramsInterface")
-    Programs.style.opacity = 0
-}
-function ToggleProjectGit(){
-    console.log("they clicked!")
-    const ROBLOX = document.getElementById("ROBLOX")
-    ROBLOX.style.opacity = 0
-    const Website = document.getElementById('WebsiteInterface')
-    Website.style.opacity = 0
-    const Programs = document.getElementById("ProgramsInterface")
-    Programs.style.opacity = 0
-    
-}
+    Programs.classList.toggle("Hidden")}
+
+
 function ToggleProgramheader(){
     console.log("they clicked5")
     const ROBLOX = document.getElementById("ROBLOX")
-    ROBLOX.style.opacity = 0
+    ROBLOX.classList.add("Hidden")
     const Website = document.getElementById('WebsiteInterface')
-    Website.style.opacity = 0
+    Website.classList.add("Hidden")
     const Programs = document.getElementById("ProgramsInterface")
-   Programs.style.opacity = 1 
+   Programs.classList.toggle("Hidden") 
 }
 function GameClicker(){
     console.log("it clicked!")
     const ToggleUi = document.getElementById("Games")
     console.log("This is the games tab", ToggleUi)
     ToggleUi.classList.toggle("Hidden")
+    const ROBLOX = document.getElementById("ROBLOX")
+    ROBLOX.classList.add("Hidden")
+    
 }
 function RobloxToggle(){
 
@@ -128,6 +115,7 @@ function RobloxToggle(){
 function UnityToggle(){
 
 }
+
 RobloxTab.addEventListener("click",ToggleRoblox)
 Games.addEventListener("click",GameClicker)
 WebsiteHeader.addEventListener("click",ToggleWebsite)
