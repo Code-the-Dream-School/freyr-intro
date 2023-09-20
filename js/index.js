815fa5542d04c26d2c10cca7a08e426672021d68
Cristian Moran <cmoran1654@gmail.com>
2023-09-20 16:30:25 -0500

    lesson-6-2

--- js/index.js
+++ js/index.js
@@ -93,23 +93,24 @@ messageForm.addEventListener("submit", function (event) {
   messageForm.reset();
 });
 
-// Github request
-const githubRequest = new XMLHttpRequest();
-githubRequest.open("GET", "https://api.github.com/users/CristianMoran1/repos");
+//Using the Fetch API, create a "GET" request to the same GitHub API url as before
+fetch("https://api.github.com/users/CristianMoran1/repos", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (repositories) {
+    // Display repositories in list
+    const projectSection = document.getElementById("projects");
+    const projectList = projectSection.querySelector("ul");
 
-githubRequest.addEventListener("load", function (event) {
-  const repositories = JSON.parse(githubRequest.responseText);
-  console.log(repositories);
-
-  // Display repositories in list
-  const projectSection = document.getElementById("projects");
-  const projectList = projectSection.querySelector("ul");
-
-  for (let i = 0; i < repositories.length; i++) {
-    const project = document.createElement("li");
-    project.innerText = repositories[i].name;
-    projectList.appendChild(project);
-  }
-});
-
-githubRequest.send();
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li");
+      project.innerText = repositories[i].name;
+      projectList.appendChild(project);
+    }
+  })
+  .catch(function (error) {
+    console.error("Fetch error:", error);
+  });
