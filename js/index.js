be305614479e8dc8d689e0e76f8617a65444c451
themagicianking <tmoonder@gmail.com>
2023-11-04 23:57:02 -0400

    Added a submission form for messages.

--- js/index.js
+++ js/index.js
@@ -16,3 +16,66 @@ for (let i = 0; i <= skills.length; i++) {
   skill.innerHTML = skills[i];
   skillsList.appendChild(skill);
 };
+
+messageForm = document.querySelector('[name="leave_message"]');
+messageForm.addEventListener("submit", function () {
+  event.preventDefault();
+
+  name = event.target.usersName.value;
+  email = event.target.usersEmail.value;
+  message = event.target.usersMessage.value;
+
+  console.log(name + " " + email + " " + message);
+
+  messageSection = document.getElementById("messages");
+  if (messageSection.querySelector("ul").innerHTML.trim() == "") {
+    messageHeader = document.createElement("h2");
+    messageHeader.innerHTML = "Messages";
+    messageSection.prepend(messageHeader);
+  };
+  messageList = messageSection.querySelector("ul");
+  newMessage = document.createElement("li");
+  
+  mailLink = document.createElement("a");
+  mailLink.innerHTML = name;
+  mailLink.href = `mailto:${email}`;
+
+  messageDisplay = document.createElement("span");
+  messageDisplay.innerHTML = message;
+
+  newMessage.appendChild(mailLink);
+  newMessage.appendChild(messageDisplay);
+
+  editButton = document.createElement("button");
+  editButton.innerText = "edit";
+  editButton.type = "button";
+
+  editButton.addEventListener("click", function () {
+    self = this;
+    currentMessage = self.parentNode.querySelector("span").textContent;
+    editedMessage = prompt("Please edit your message:", `${currentMessage}`);
+    if (editedMessage == null) {
+      editedMessage = currentMessage;
+    };
+    self.parentNode.querySelector("span").innerHTML = editedMessage;
+  });
+  
+  removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+
+  removeButton.addEventListener("click", function () {
+    self = this;
+    entry = self.parentNode;
+    entry.remove();
+    if (messageSection.querySelector("ul").innerHTML.trim() == "") {
+      messageHeader.remove();
+    };
+  });
+
+  newMessage.appendChild(editButton);
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageForm.reset();
+});
