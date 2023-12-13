725ba39b196a5bc8ad113ced89291314579f0e90
kirt-desai <100534639+kirt-desai@users.noreply.github.com>
2023-12-12 20:27:37 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+const today = new Date('December 12, 2023, 5:05:00');
+
+var thisYear = today.getFullYear();
+const footer = document.querySelector("#footer");
+var copyright = document.createElement("p");
+
+let copyright = document.getElementById(thisYear).innerHTML;
+footer.appendChild(copyright);
+
 
