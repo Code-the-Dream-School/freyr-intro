0d0340e3d6d392c2c8fcb548150bd485f45880bb
kirt-desai <100534639+kirt-desai@users.noreply.github.com>
2023-12-13 10:53:49 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,15 @@ var thisYear = today.getFullYear();
 const footer = document.querySelector("#footer");
 var copyright = document.createElement("p");
 
-let copyright = document.getElementById(thisYear).innerHTML;
+const copyright = document.getElementById(thisYear).innerHTML;
 footer.appendChild(copyright);
 
+const skills = [ "Java", "Python", "Javascript", "HTML", "CSS", "Microsoft Excel Level 3", "Access Level 1", "Powerpoint", "Microsoft Azure", "Tableau", "Linux"];
+const skillsSection = document.querySelector("#skills");
+const skillsList = skillsSection.querySelector('ul');
 
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
